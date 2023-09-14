7d976785ba2cb0f824cb4bcb0d481150e37661c3
Elisa Lenotre <ele23006@open-groupe.com>
2023-09-14 11:23:22 +0200

    part3 nav + contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+// app.module.ts
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterComponent } from './footer/footer.component';
 import { MessageService } from './message.service';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -14,10 +18,13 @@ import { MessageService } from './message.service';
     HeaderComponent,
     MainContentComponent,
     FooterComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule,
+    AppRoutingModule,
   ],
   providers: [MessageService],
   bootstrap: [AppComponent],
